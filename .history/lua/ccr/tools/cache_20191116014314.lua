1bd2677bb600c366889701e011d2c71bf371ca9f
CupCakeR <27384135+CupCakeR@users.noreply.github.com>
2020-01-27 22:12:29 +0100

    Deleted .history/ files

--- .history/lua/ccr/tools/cache_20191116014314.lua
+++
@@ -1,9 +0,0 @@
-
-CCR_CACHE_TARGET = nil
-function CCR:SetCacheTarget(tbl, id)
-	CCR_CACHE_TARGET = tbl
-end
-
-function CCR:ResetCacheTarget(tbl)
-	tbl.CCR_CACHE = nil
-end
--- .history/lua/ccr/tools/misc_20191103174549.lua
+++
@@ -1,8 +0,0 @@
-
-function CCR:RandomString(length)
-	local res = ""
-	for i = 1, length do
-		res = res .. string.char(math.random(97, 122))
-	end
-	return res
-end
