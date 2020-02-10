d341e8085e58164eaddc63ddf0f4b34f2dca349f
CupCakeR <27384135+CupCakeR@users.noreply.github.com>
2020-02-10 19:12:01 +0100

    Added PANEL.LerpPos

--- lua/ccr/tools/anim.lua
+++ lua/ccr/tools/anim.lua
@@ -38,7 +38,17 @@ CCR:AddPanelFunction("LerpSize", function(s, w, h, dur, callback)
 		local _w = Lerp(new_frac, _s.from_w, _s.to_w)
 		local _h = Lerp(new_frac, _s.from_h, _s.to_h)
 
-		s:SetSize(_w, _h)
+CCR:AddPanelFunction("LerpPos", function(s, x, y, dur, callback)
+	local anim = s:NewAnimation(dur or .1)
+	anim.from_x, anim.from_y = s:GetPos()
+	anim.to_x = x
+	anim.to_y = y
+	anim.Think = function(_s, pnl, frac)
+		local new_frac = Lerp(frac, 0, 1)
+		local _x = Lerp(new_frac, _s.from_x, _s.to_x)
+		local _y = Lerp(new_frac, _s.from_y, _s.to_y)
+
+		pnl:SetPos(_x, _y)
 	end
 	anim.OnEnd = function(_s)
 		if (callback) then
