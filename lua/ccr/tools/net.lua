c5cfce1b90fe14f468d10bef3d4c0c2b51f69da4
t.wruck <t.wruck@bucs-it.de>
2020-02-11 16:14:40 +0100

    Added spam detection

--- lua/ccr/tools/net.lua
+++ lua/ccr/tools/net.lua
@@ -1,23 +1,47 @@
 
-function CCR:NetReceiveDebounced(id, cb, dataFunc, debounceTime)
+CCR.NetSpam = CCR.NetSpam || {}
+
+function CCR:NetReceiveDebounced(id, cb, dataFunc, options)
 	assert(id, "No ID specified")
 	assert(isfunction(cb), "No callback specified")
 
+	options = options || {}
+	options.debounceTime = options.debounceTime || 0
+	options.spam = options.spam == nil && 10 || tonumber(options.spam)
+
 	dataFunc = dataFunc || function() end
-	debounceTime = debounceTime || 0
 
 	net.Receive(id, function(len, p)
+		self.NetSpam[p] = self.NetSpam[p] || {}
+		self.NetSpam[p][id] = (self.NetSpam[p][id] || 0) + 1
+
+		if (options.spam && options.spam >= self.NetSpam[p][id]) then
+			if (options.spam == self.NetSpam[p][id]) then
+				local str = ":nick (:sid) exceeded the spam limit of :limit of the net message \":id\". This could could be an ongoing attempt to crash the server."
+				str = str:Replace(":nick", p:Nick())
+				str = str:Replace(":sid", p:SteamID())
+				str = str:Replace(":id", id)
+				str = str:Replace(":limit", options.spam)
+
+				error(str)
+			end
+
+			return
+		end
+
 		local data = {}
 		dataFunc(data)
 
-		CCR:Debounce("CCR.Net[" .. id .. "][" .. p:SteamID() .. "]", debounceTime, function()
+		CCR:Debounce("CCR.Net[" .. id .. "][" .. p:SteamID() .. "]", options.debounceTime, function()
 			if (!IsValid(p)) then
 				return
 			end
+
+			self.NetSpam[p][id] = nil
 
 			cb(p, data, len)
 		end)
 	end)
 end
 
-// TODO: Spam protected variant (stack? Call instantly if stack is empty)
+// TODO: Spam protected variant (stack? Call instantly if stack is empty) 
